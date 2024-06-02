0564e46e2651d4e407e217e93074c015deb9376f
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-06-02 18:18:27 +0530

    Adding Extra Components For Reusability & Configurability

---
+++ src/components/Accordion/AccordionTitle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useAccordionContext } from "./Accordion.jsx";
+
+export default function AccordionTitle({ id, className, children }) {
+  const { toggleItem } = useAccordionContext();
+
+  return (
+    <h3 className={className} onClick={() => toggleItem(id)}>
+      {children}
+    </h3>
+  );
+}
